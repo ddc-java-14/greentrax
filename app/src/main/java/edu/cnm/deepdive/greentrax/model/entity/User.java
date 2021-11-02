f95b9de2e6e636626ed3733dc0047cf458f5e6c8
zcr003 <zramsey1@cnm.edu>
2021-11-02 09:02:11 -0600

    Formatting and changing imports to room. (in progress).

--- app/src/main/java/edu/cnm/deepdive/greentrax/model/entity/User.java
+++ app/src/main/java/edu/cnm/deepdive/greentrax/model/entity/User.java
@@ -8,19 +8,6 @@ import androidx.room.PrimaryKey;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.UUID;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.OrderBy;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 
 @Entity(
@@ -46,37 +33,31 @@ public class User {
   @ColumnInfo(index = true)
   private Date created;
 
-  @Column(nullable = false, updatable = false, unique = true, length = 30)
-  private String oauthKey;
 
-  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY,
-      cascade = CascadeType.ALL, orphanRemoval = true)
-  @OrderBy("created DESC")
-  private final List<History> history = new LinkedList<>();
-
-  public UUID getId() {
+  public long getId() {
     return id;
   }
 
-  public UUID getExternalKey() {
-    return externalKey;
+  public void setId(long id) {
+    this.id = id;
   }
 
+  @NonNull
+  public String getServiceKey() {
+    return serviceKey;
+  }
+
+  public void setServiceKey(@NonNull String serviceKey) {
+    this.serviceKey = serviceKey;
+  }
+
+  @NonNull
   public Date getCreated() {
     return created;
   }
 
-  public String getOauthKey() {
-    return oauthKey;
-  }
-
-  public void setOauthKey(String oauthKey) {
-    this.oauthKey = oauthKey;
-  }
-
-
-  public List<History> getHistory() {
-    return history;
+  public void setCreated(@NonNull Date created) {
+    this.created = created;
   }
 }
 
